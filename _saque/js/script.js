3665e35b324b74746aced4d8616f10170a52fecd
Marrane <49756227+Henrique-Marrane@users.noreply.github.com>
2021-06-22 19:01:25 -0300

    Inicio tela de saque

---
+++ _saque/js/script.js
@@ -0,0 +1,19 @@
+function getPagamento(){
+    let formapag = $("#selected-pag").val();
+    switch(formapag){
+        case "tpix":
+            $("#conta").css("display","none");
+            $("#agencia").css("display","none");
+            $("#chavepix").css("display","block");
+            break;
+        case "ted":
+            $("#conta").css("display","block");
+            $("#agencia").css("display","block");
+            $("#chavepix").css("display","none");
+            break;
+        default:
+            $("#conta").css("display","block");
+            $("#chavepix").css("display","block");
+            $("#agencia").css("display","block");
+    }
+  }
