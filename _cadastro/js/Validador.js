d95ed048438bef438c69f1ebc82d43b08381e6b0
Marrane <49756227+Henrique-Marrane@users.noreply.github.com>
2021-07-21 12:03:31 -0300

    valida-login

--- _cadastro/js/Validador.js
+++ _cadastro/js/Validador.js
@@ -1,14 +1,37 @@
 function criaMascara(mascaraInput) {
-  const maximoInput = document.getElementById(`${mascaraInput}Input`).maxLength;
-  let valorInput = document.getElementById(`${mascaraInput}Input`).value;
-  let valorSemPonto = document.getElementById(`${mascaraInput}Input`).value.replace(/([^0-9])+/g, "");
+  const maximoInput = document.getElementById(`${mascaraInput}Input`).maxLength
+  let valorInput = document.getElementById(`${mascaraInput}Input`).value
+  let valorSemPonto = document
+    .getElementById(`${mascaraInput}Input`)
+    .value.replace(/([^0-9])+/g, '')
   const mascaras = {
-    CPF: valorInput.replace(/[^\d]/g, "").replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4"),
-    Celular: valorInput.replace(/[^\d]/g, "").replace(/^(\d{2})(\d{5})(\d{4})/, "($1) $2-$3"),
-    CEP: valorInput.replace(/[^\d]/g, "").replace(/(\d{5})(\d{3})/, "$1-$2"),
-    CNJ: valorInput.replace(/[^\d]/g, "").replace(/(\d{7})(\d{2})(\d{4})(\d{1})(\d{2})(\d{4})/, "$1-$2.$3.$4.$5.$6"),
-  };
+    CPF: valorInput
+      .replace(/[^\d]/g, '')
+      .replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4'),
+    Celular: valorInput
+      .replace(/[^\d]/g, '')
+      .replace(/^(\d{2})(\d{5})(\d{4})/, '($1) $2-$3'),
+    CEP: valorInput.replace(/[^\d]/g, '').replace(/(\d{5})(\d{3})/, '$1-$2'),
+    CNJ: valorInput
+      .replace(/[^\d]/g, '')
+      .replace(
+        /(\d{7})(\d{2})(\d{4})(\d{1})(\d{2})(\d{4})/,
+        '$1-$2.$3.$4.$5.$6'
+      )
+  }
 
-  valorInput.length === maximoInput ? document.getElementById(`${mascaraInput}Input`).value = mascaras[mascaraInput]
- : document.getElementById(`${mascaraInput}Input`).value = valorSemPonto;
-};
+  valorInput.length === maximoInput
+    ? (document.getElementById(`${mascaraInput}Input`).value =
+        mascaras[mascaraInput])
+    : (document.getElementById(`${mascaraInput}Input`).value = valorSemPonto)
+}
+
+document.getElementById('login').onkeypress = function (e) {
+  var chr = String.fromCharCode(e.which)
+  if (
+    '1234567890qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM'.indexOf(
+      chr
+    ) < 0
+  )
+    return false
+}
---
+++ _cadastro/js/validalogin.js
@@ -0,0 +1,9 @@
+adocument.getElementById('login').onkeypress = function (e) {
+  var chr = String.fromCharCode(e.which)
+  if (
+    '1234567890qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM'.indexOf(
+      chr
+    ) < 0
+  )
+    return false
+}
